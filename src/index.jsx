ee63879069c129811b8dae4ca9bd37b062f50ac1
Florian Behrens <florian.behrens@rocketmail.com>
2019-03-21 12:53:10 +0100

    First react based build

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import ReactDom from 'react-dom';
+
+const root = document.getElementById("react-root");
+ReactDom.render(<div>
+    <p>Welcome!</p>
+</div>, root);
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.jsx',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname),
+        filename: 'bundle.js'
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+};
